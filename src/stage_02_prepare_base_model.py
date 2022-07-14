1b4242598ceb6c143e52168a33a61be594781dd4
sanjeev kumar <sanjeevkr4983@gmail.com>
2022-07-14 15:21:34 +0530

    main file crated and added params in params.yaml

--- src/stage_02_prepare_base_model.py
+++ src/stage_02_prepare_base_model.py
@@ -18,6 +18,19 @@ def prepare_base_model(config_path, params_path):
     config = read_yaml(config_path)
     params = read_yaml(params_path)
 
+    artifacts = config["artifacts"]
+    artifacts_dir = artifacts["ARTIFACTS_DIR"]
+
+    base_model_dir = artifacts["BASE_MODEL_DIR"]
+    base_model_name = artifacts["BASE_MODEL_NAME"]
+    base_model_dir_path = os.path.join(artifacts_dir, base_model_dir)
+
+    create_directories([base_model_dir_path])
+
+    base_model_path = os.path.join(base_model_dir_path, base_model_name)
+    model = get_VGG_16_model(input_shape=params["IMAGE_SIZE"], model_path)
+
+
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
