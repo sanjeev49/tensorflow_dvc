13b43f891df3b3a3867304c725acaa43b05b7ec9
sanjeev kumar <sanjeevkr4983@gmail.com>
2022-07-19 14:37:38 +0530

    base file ready

--- src/stage_03_prepare_callbacks.py
+++ src/stage_03_prepare_callbacks.py
@@ -3,7 +3,7 @@ import os
 import logging
 from src.utils.common import read_yaml, create_directories
 from src.utils.models import get_vgg16_model, prepare_model
-import io
+from src.utils.callbacks import create_and_save_tensorboard_callback, create_and_save_checkpoint_callback
 
 STAGE = "prepare_callbacks"
 
@@ -15,6 +15,26 @@ logging.basicConfig(
 )
 
 def prepare_callbacks(config_path, params_path):
+    config = read_yaml(config_path)
+    
+    artifacts = config["artifacts"]
+    artifacts_dir = artifacts["ARTIFACTS_DIR"]
+    tensorboard_log_dir = os.path.join(artifacts_dir, artifacts["TENSORBOARD_ROOT_LOG_DIR"])
+    checkpoint_dir = os.path.join(artifacts_dir, artifacts["CHECKPOINT_DIR"])
+    callback_dir = os.path.join(artifacts_dir, artifacts["CALLBACKS_DIR"])
+
+    # creating all the necessary directories 
+    create_directories([
+        tensorboard_log_dir,
+        checkpoint_dir,
+        callback_dir
+    ])
+
+    create_and_save_tensorboard_callback(callback_dir, tensorboard_log_dir)
+    create_and_save_checkpoint_callback(callback_dir, tensorboard_log_dir)
+
+
+
 
 
 if __name__ == '__main__':
